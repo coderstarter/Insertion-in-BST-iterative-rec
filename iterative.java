2e7d32cbfd142210c0631c463ec8b3485ad5a44e
coderstarter <102434201+coderstarter@users.noreply.github.com>
2022-10-19 02:11:40 +0530

    Create iterative.java

---
+++ iterative.java
@@ -0,0 +1,69 @@
+class binarySearchTree {
+
+	private Node root;
+
+	public Node getRoot() {
+		return root;
+    }
+    
+	public void setRoot(Node root) {
+		this.root = root;
+	}
+
+	//Iterative Function to insert a value in BST
+	public boolean add(int value) {
+
+		//If Tree is empty then insert Root with the given value inside Tree   
+		if (isEmpty()) {
+			root = new Node(value);
+			return true;
+		}
+
+		//Starting from root
+		Node currentNode = root;
+
+		//Traversing the tree untill valid position to insert the value
+		while (currentNode != null) {
+
+			Node leftChild = currentNode.getLeftChild();
+			Node rightChild = currentNode.getRightChild();
+
+			//If the value to insert is less than root value then move to left subtree
+			//else move to right subtree of root
+			//and before moving check if the subtree is null, if it's then insert the value.
+			if (currentNode.getData() > value) {
+				if (leftChild == null) {
+					leftChild = new Node(value);
+					currentNode.setLeftChild(leftChild);
+					return true;
+				}
+				currentNode = leftChild;
+			} else {
+				if (rightChild == null) {
+					rightChild = new Node(value);
+					currentNode.setRightChild(rightChild);
+					return true;
+				}
+				currentNode = rightChild;
+			} //end of else
+		} //end of while
+		return false;
+	}
+
+	//Function to check if Tree is empty or not  
+	public boolean isEmpty() 
+    {
+		return root == null; //if root is null then it means Tree is empty
+	}
+
+	//Just for Testing purpose 
+	public void printTree(Node current) 
+    {
+		if (current == null) return;
+
+		System.out.print(current.getData() + ",");
+		printTree(current.getLeftChild());
+		printTree(current.getRightChild());
+
+	}
+}
